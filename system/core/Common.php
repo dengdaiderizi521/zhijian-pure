bd17db5fcd1007454f5ffbd6c27c786766551761
程远皓 <chengyuanhao@51leho.cn>
2016-04-07 11:10:42 +0800

    增加获取页面参数方法

--- system/core/Common.php
+++ system/core/Common.php
@@ -6,22 +6,23 @@
 |---------------------------------------------------------------
  */
 
-define('CONFIGPATH',APPPATH.'config/');
+define('CONFIGPATH', APPPATH . 'config/');
 
 /*
 |---------------------------------------------------------------
 | 获取用户配置
 |---------------------------------------------------------------
  */
-function _get_config($filename='config',$key=FALSE){
-	if(!file_exists(CONFIGPATH.$filename.'.php')){
-		die("配置文件未找到");
-	}
-	require CONFIGPATH.$filename.'.php';
-	if(!$key){
-		return $config;
-	}
-	return isset($config[$key])?$config[$key]:FALSE;
+function _get_config($filename = 'config', $key = false)
+{
+    if (!file_exists(CONFIGPATH . $filename . '.php')) {
+        die("配置文件未找到");
+    }
+    require CONFIGPATH . $filename . '.php';
+    if (!$key) {
+        return $config;
+    }
+    return isset($config[$key]) ? $config[$key] : false;
 }
 
 /*
@@ -30,40 +31,40 @@ function _get_config($filename='config',$key=FALSE){
 |---------------------------------------------------------------
  */
 
-function _error_log($errno, $errstr, $filepath, $line){
-	$levels = array(
-		E_ERROR				=>	'Error',
-		E_WARNING			=>	'Warning',
-		E_PARSE				=>	'Parsing Error',
-		E_NOTICE			=>	'Notice',
-		E_CORE_ERROR		=>	'Core Error',
-		E_CORE_WARNING		=>	'Core Warning',
-		E_COMPILE_ERROR		=>	'Compile Error',
-		E_COMPILE_WARNING	=>	'Compile Warning',
-		E_USER_ERROR		=>	'User Error',
-		E_USER_WARNING		=>	'User Warning',
-		E_USER_NOTICE		=>	'User Notice',
-		E_STRICT			=>	'Runtime Notice'
-	);
-	if ($errno == E_STRICT)
-	{
-		die;
-	}
-	$log_path = _get_config('config','log_path');
-	if(empty($log_path)){
-		die;
-	}
-	if(!is_dir($log_path)){
-		@mkdir($log_path,0777);
-	}
-	$log_path .= '/error-'.date('Y-m-d').'.log';
-	$errormessage = '';
-	if(file_exists($log_path)){
-		$errormessage = file_get_contents($log_path);
-	}
-	$errormessage .= date('Y-m-d H:i:s').' '.$levels[$errno].':'.strip_tags($errstr).' in '.$filepath.' on line '.$line.' uri:'.$_SERVER['REQUEST_URI']."\n";
-	file_put_contents($log_path,$errormessage);
-	die;
+function _error_log($errno, $errstr, $filepath, $line)
+{
+    $levels = array(
+        E_ERROR => 'Error',
+        E_WARNING => 'Warning',
+        E_PARSE => 'Parsing Error',
+        E_NOTICE => 'Notice',
+        E_CORE_ERROR => 'Core Error',
+        E_CORE_WARNING => 'Core Warning',
+        E_COMPILE_ERROR => 'Compile Error',
+        E_COMPILE_WARNING => 'Compile Warning',
+        E_USER_ERROR => 'User Error',
+        E_USER_WARNING => 'User Warning',
+        E_USER_NOTICE => 'User Notice',
+        E_STRICT => 'Runtime Notice'
+    );
+    if ($errno == E_STRICT) {
+        die;
+    }
+    $log_path = _get_config('config', 'log_path');
+    if (empty($log_path)) {
+        die;
+    }
+    if (!is_dir($log_path)) {
+        @mkdir($log_path, 0777);
+    }
+    $log_path .= '/error-' . date('Y-m-d') . '.log';
+    $errormessage = '';
+    if (file_exists($log_path)) {
+        $errormessage = file_get_contents($log_path);
+    }
+    $errormessage .= date('Y-m-d H:i:s') . ' ' . $levels[$errno] . ':' . strip_tags($errstr) . ' in ' . $filepath . ' on line ' . $line . ' uri:' . $_SERVER['REQUEST_URI'] . "\n";
+    file_put_contents($log_path, $errormessage);
+    die;
 }
 
 /*
@@ -72,34 +73,35 @@ function _error_log($errno, $errstr, $filepath, $line){
 |---------------------------------------------------------------
 */
 
-function _no_rewrite_run($controller,$function){
-	if(!isset($_GET[$controller])){
-		$default_controller = _get_config('config','default_controller');
-		if(!$default_controller){
-			_page_error_404();
-		}
-		$class = ucfirst(strtolower($default_controller));
-	}else{
-		$class = ucfirst(strtolower($_GET[$controller]));
-	}
-	if(!isset($_GET[$function])){
-		$function = 'index';
-	}else{
-		$function = strtolower($_GET[$function]);
-	}
-	if(!file_exists(APPPATH.'controller/'.$class.'.php')){
-		_page_error_404();
-	}
-	require_once SYSTEMPATH.'core/Controller.php';
-	require_once CONTROLLERPATH.$class.'.php';
-	if(!class_exists($class)){
-		_page_error_404();
-	}
-	$obj = new $class();
-	if(!method_exists($obj,$function)){
-		_page_error_404();
-	}
-	$obj->$function();
+function _no_rewrite_run($controller, $function)
+{
+    if (!isset($_GET[$controller])) {
+        $default_controller = _get_config('config', 'default_controller');
+        if (!$default_controller) {
+            _page_error_404();
+        }
+        $class = ucfirst(strtolower($default_controller));
+    } else {
+        $class = ucfirst(strtolower($_GET[$controller]));
+    }
+    if (!isset($_GET[$function])) {
+        $function = 'index';
+    } else {
+        $function = strtolower($_GET[$function]);
+    }
+    if (!file_exists(APPPATH . 'controller/' . $class . '.php')) {
+        _page_error_404();
+    }
+    require_once SYSTEMPATH . 'core/Controller.php';
+    require_once CONTROLLERPATH . $class . '.php';
+    if (!class_exists($class)) {
+        _page_error_404();
+    }
+    $obj = new $class();
+    if (!method_exists($obj, $function)) {
+        _page_error_404();
+    }
+    $obj->$function();
 }
 
 /*
@@ -107,39 +109,40 @@ function _no_rewrite_run($controller,$function){
 | 伪静态下执行请求
 |---------------------------------------------------------------
 */
-function _rewrite_run(){
-	$uriArr = _fetch_uri_string();
-	if(!isset($uriArr['controller'])){
-		$default_controller = _get_config('config','default_controller');
-		if(!$default_controller){
-			_page_error_404();
-		}
-		$class = ucfirst(strtolower($default_controller));
-	}else{
-		$class = ucfirst(strtolower($uriArr['controller']));
-	}
-	if(!isset($uriArr['function'])){
-		$function = 'index';
-	}else{
-		$function = strtolower($uriArr['function']);
-	}
-	if(!file_exists(APPPATH.'controller/'.$class.'.php')){
-		_page_error_404();
-	}
-	require_once SYSTEMPATH.'core/Controller.php';
-	require_once CONTROLLERPATH.$class.'.php';
-	if(!class_exists($class)){
-		_page_error_404();
-	}
-	$obj = new $class();
-	if(!method_exists($obj,$function)){
-		_page_error_404();
-	}
-	if(!empty($uriArr['params'])){
-		call_user_func_array(array($obj, $function),$uriArr['params']);
-	}else{
-		$obj->$function();
-	}
+function _rewrite_run()
+{
+    $uriArr = _fetch_uri_string();
+    if (!isset($uriArr['controller'])) {
+        $default_controller = _get_config('config', 'default_controller');
+        if (!$default_controller) {
+            _page_error_404();
+        }
+        $class = ucfirst(strtolower($default_controller));
+    } else {
+        $class = ucfirst(strtolower($uriArr['controller']));
+    }
+    if (!isset($uriArr['function'])) {
+        $function = 'index';
+    } else {
+        $function = strtolower($uriArr['function']);
+    }
+    if (!file_exists(APPPATH . 'controller/' . $class . '.php')) {
+        _page_error_404();
+    }
+    require_once SYSTEMPATH . 'core/Controller.php';
+    require_once CONTROLLERPATH . $class . '.php';
+    if (!class_exists($class)) {
+        _page_error_404();
+    }
+    $obj = new $class();
+    if (!method_exists($obj, $function)) {
+        _page_error_404();
+    }
+    if (!empty($uriArr['params'])) {
+        call_user_func_array(array($obj, $function), $uriArr['params']);
+    } else {
+        $obj->$function();
+    }
 }
 
 /*
@@ -147,34 +150,39 @@ function _rewrite_run(){
 | 获取分割后的URI
 |---------------------------------------------------------------
 */
-function _fetch_uri_string(){
-	if(empty($_GET['uri'])) return FALSE;
-	$uriStr = htmlspecialchars($_GET['uri']);
-	$url_suffix = _get_config('config','url_suffix');
-	if(!empty($url_suffix)){
-		$uriStr = preg_replace('/^(.*)'.preg_quote($url_suffix).'$/', '$1', $uriStr);
-	}
-	$tmpArr = explode('/', $uriStr);
-	$uriArr['controller'] = $tmpArr[0];
-	unset($tmpArr[0]);
-	if(!isset($tmpArr[1])){
-		$uriArr['function'] = 'index';
-		return $uriArr;
-	}
-	$uriArr['params'] = array();
-	if((int)$tmpArr[1]!=0){
-		$uriArr['function'] = 'index';
-		$uriArr['params'][0] = $tmpArr[1];
-		unset($tmpArr[1]);
-	}else{
-		$uriArr['function'] = $tmpArr[1];
-		unset($tmpArr[1]);
-	}
-	if(empty($tmpArr)) return $uriArr;
-	foreach ($tmpArr as $k => $v) {
-		$uriArr['params'][count($uriArr['params'])] = $v;
-	}
-	return $uriArr;
+function _fetch_uri_string()
+{
+    if (empty($_GET['uri'])) {
+        return false;
+    }
+    $uriStr = htmlspecialchars($_GET['uri']);
+    $url_suffix = _get_config('config', 'url_suffix');
+    if (!empty($url_suffix)) {
+        $uriStr = preg_replace('/^(.*)' . preg_quote($url_suffix) . '$/', '$1', $uriStr);
+    }
+    $tmpArr = explode('/', $uriStr);
+    $uriArr['controller'] = $tmpArr[0];
+    unset($tmpArr[0]);
+    if (!isset($tmpArr[1])) {
+        $uriArr['function'] = 'index';
+        return $uriArr;
+    }
+    $uriArr['params'] = array();
+    if ((int)$tmpArr[1] != 0) {
+        $uriArr['function'] = 'index';
+        $uriArr['params'][0] = $tmpArr[1];
+        unset($tmpArr[1]);
+    } else {
+        $uriArr['function'] = $tmpArr[1];
+        unset($tmpArr[1]);
+    }
+    if (empty($tmpArr)) {
+        return $uriArr;
+    }
+    foreach ($tmpArr as $k => $v) {
+        $uriArr['params'][count($uriArr['params'])] = $v;
+    }
+    return $uriArr;
 }
 
 /*
@@ -182,10 +190,11 @@ function _fetch_uri_string(){
 | 404错误处理
 |---------------------------------------------------------------
 */
-function _page_error_404(){
-	header('HTTP/1.1 404 Not Found');
-	if(file_exists(APPPATH.'error/404.php')){
-		require_once APPPATH.'error/404.php';
-	}
-	die;
+function _page_error_404()
+{
+    header('HTTP/1.1 404 Not Found');
+    if (file_exists(APPPATH . 'error/404.php')) {
+        require_once APPPATH . 'error/404.php';
+    }
+    die;
 }
--- system/core/ZhiJian.php
+++ system/core/ZhiJian.php
@@ -4,7 +4,7 @@
 | 载入框架公共文件
 |---------------------------------------------------------------
  */
-require_once SYSTEMPATH.'core/Common.php';
+require_once SYSTEMPATH . 'core/Common.php';
 /*
 |---------------------------------------------------------------
 | 获取用户配置信息
@@ -17,13 +17,13 @@ $config = _get_config();
 | 定义项目文件目录
 |---------------------------------------------------------------
  */
-define('CONTROLLERPATH',APPPATH.'controller/');
-define('VIEWPATH',APPPATH.'view/');
-define('MODELPATH',APPPATH.'model/');
-if(empty($config['view_file_suffix'])){
-	define('VIEWEXT','.php');
-}else{
-	define('VIEWEXT',$config['view_file_suffix']);
+define('CONTROLLERPATH', APPPATH . 'controller/');
+define('VIEWPATH', APPPATH . 'view/');
+define('MODELPATH', APPPATH . 'model/');
+if (empty($config['view_file_suffix'])) {
+    define('VIEWEXT', '.php');
+} else {
+    define('VIEWEXT', $config['view_file_suffix']);
 }
 
 /*
@@ -31,26 +31,25 @@ if(empty($config['view_file_suffix'])){
 | debug处理
 |---------------------------------------------------------------
 */
-if(!$config['debug']){
-	if($config['error_log']&&!empty($config['log_path'])){
-		set_error_handler('_error_log');
-	}else{
-		error_reporting(NULL);
-	}
+if (!$config['debug']) {
+    if ($config['error_log'] && !empty($config['log_path'])) {
+        set_error_handler('_error_log');
+    } else {
+        error_reporting(null);
+    }
 }
 /*
 |---------------------------------------------------------------
 | 执行请求
 |---------------------------------------------------------------
 */
-if(!$config['rewrite']){
-	_no_rewrite_run($config['controller_trigger'],$config['function_trigger']);
-}else{
-	$result = apache_get_modules();
-	if(!in_array('mod_rewrite', $result)){
-		_no_rewrite_run($config['controller_trigger'],$config['function_trigger']);
-	}else{
-		_rewrite_run();
-	}
+if (!$config['rewrite']) {
+    _no_rewrite_run($config['controller_trigger'], $config['function_trigger']);
+} else {
+    $result = apache_get_modules();
+    if (!in_array('mod_rewrite', $result)) {
+        _no_rewrite_run($config['controller_trigger'], $config['function_trigger']);
+    } else {
+        _rewrite_run();
+    }
 }
-?>
